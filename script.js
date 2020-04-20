13b2edfd00244f9efd42578b186b199f12031c43
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-04-21 03:51:13 +0500

    compulte j.s

--- script.js
+++ script.js
@@ -1,67 +1,66 @@
-const video = document.getElementById("video");
-const play = document.getElementById("play");
-const stop = document.getElementById("stop");
-const progress = document.getElementById("progress");
-const timestamp = document.getElementById("timestamp");
+  
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
 
-//play & pause video
+// Play & pause video
 function toggleVideoStatus() {
-   if (video.pause) {
-       video.play();
-   } else {
-       video.pause();
-   }
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
 // update play/pause icon
-function updatePlayIcon(){
-   if(video.pause) {
-       play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
-   } else {
-       play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
-   }
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
 }
 
+// Update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
 
-// update progress & timestamp 
-function updateProgress(){
-   progress.value = (video.currentTime / video.duration) *
-   100;
-   //get minutes
-   let mins = math.floor(video.currentTime / 60);
-   if (mins <10) {
-       mins = '0' + String(mins);
-   }
-   //get seconds
-   let secs = math.floor(video.currentTime / 60);
-   if (secs <10) {
-       mins = '0' + String(mins);
-   }
-   timestamp.innerHTML = '${mins}:${secs}';
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
-//set video time to progress
-function setVideoProgress(){
- video
+// Set video time to progress
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
-//stop video 
-function stopVideo(){
-video.currentTime = 0;
-video.pause();
+// Stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
 }
 
-// event listeners
-video.addEventListener("click", toggleVideoStatus);
-video.addEventListener("pause", updatePlayIcon);
-video.addEventListener("play", updatePlayIcon);
-video.addEventListener("timeupdate",updateProgress);
- 
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
 stop.addEventListener('click', stopVideo);
 
 progress.addEventListener('change', setVideoProgress);
-
-
